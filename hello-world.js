5acb9a5122b56e845ebacc02550001e5827fd17b
InstaNANO <52311496+instanano@users.noreply.github.com>
2020-01-17 13:50:58 +0530

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,21 +1,3 @@
 <script>
-document.addEventListener("DOMContentLoaded",onkeyup= function add(){
-var a,b,c,z;
-a=Number(document.getElementById("text1").value);
-b=Number(document.getElementById("text2").value);
-c=Number(document.getElementById("text3").value); 
-z= b*c/(2*Math.sin(a*11/1260)); 
-document.getElementById('text99').value = z.toFixed(2);
-})
+alert("I am an alert box!");
 </script>
-</head>
-<body>
-<p>
-Peak Position (2θ)<input id="text1" value="31.8" style="float:right;width:40%;font-Size:18px;">
-<p>
-X-Ray Wavelength <input id="text2" value="0.15418" style="float:right;width:40%;font-Size:18px;">
-<p>
-Order of Reflection (n) <input id="text3" value="1" style="float:right;width:40%;font-Size:18px;">
-<p>
-<br>
-<h2>Results</h2> <p><input id="text99" style="text-align:center;color:red;font-size:25px;width:50%" readonly>nm
